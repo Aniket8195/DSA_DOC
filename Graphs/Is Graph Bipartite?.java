90f67aafcfa167e0b2f30a8ff5b7c7dd4b676d11
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-03-21 01:03:42 +0530

    Create Is Graph Bipartite?.java

---
+++ Graphs/Is Graph Bipartite?.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int[] visited=new int[graph.length];
+        for(int i=0;i<visited.length;i++){
+            visited[i]=-1;
+        }
+        for(int i=0;i<visited.length;i++){
+            if(visited[i]==-1){
+                if(!bfs(visited,i,graph)){
+                    return false;
+
+                }
+            }
+        }
+
+        return true;
+    }
+    public boolean bfs(int[] visited,int start,int[][] graph){
+        Queue<Integer> q=new LinkedList<>();
+        visited[start]=0;
+        q.add(start);
+        while(!q.isEmpty()){
+            int node =q.poll();
+            for(int help : graph[node] ){
+                if(visited[help]==-1){
+                    if(visited[node]==1){
+                        visited[help]=0;
+                    }
+                    else{
+                        visited[help]=1;
+                    }
+                    q.add(help);
+                }
+                else if(visited[help]==visited[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
