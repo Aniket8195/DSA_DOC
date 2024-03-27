5fbb0b9f50f65618da14a9f4e79ea8da8b6e0e91
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-03-27 15:21:21 +0530

    Create topo_dfs.java

---
+++ Graphs/topo_dfs.java
@@ -0,0 +1,26 @@
+static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+    int[] visited= new int[V];
+    Stack<Integer> st= new Stack<>();
+    for (int i = 0; i < V; i++) {
+        if (visited[i]==0){
+            topoDFS(i, visited, adj, st);
+        }
+    }
+
+    int[] topo= new int[V];
+    int ind=0;
+    while (!st.isEmpty()){
+        topo[ind++]=st.pop();
+    }
+    return topo;
+}
+
+private static void topoDFS(int node, int[] visited, ArrayList<ArrayList<Integer>> adj, Stack<Integer> st) {
+    visited[node]=1;
+    for (Integer it : adj.get(node)){
+        if (visited[it]==0){
+            topoDFS(it, visited, adj, st);
+        }
+    }
+    st.push(node);
+}
