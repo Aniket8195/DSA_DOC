76eabeeca10a2f30b6e2f32ac214a51a609cdc61
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-04-04 19:31:27 +0530

    Create bubble sort.java

---
+++ Sorting/bubble sort.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        boolean swapped;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            swapped=false;
+            for(int j=0;j<n-i-1;j++){
+               if(nums[j]>nums[j+1]){
+                   int temp=nums[j];
+                   nums[j]=nums[j+1];
+                   nums[j+1]=temp;
+                   swapped=true;
+               }
+            }
+            if(swapped==false){
+                    break;
+            }
+        }
+    }
+}
