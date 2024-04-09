e5db5f5f7813e8df434ab387d2d87748664e3227
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-04-09 11:44:18 +0530

    Create Move Zeroes.java

---
+++ Sorting/Move Zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] a) {
+        
+         int j = -1;
+         int n=a.length;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        if (j == -1) return;
+        for (int i = j + 1; i < n; i++) {
+            if (a[i] != 0) {
+                
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                j++;
+            }
+        }
+        
+    }
+}
