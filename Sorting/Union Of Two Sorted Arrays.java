0953f2b2185e402c7ef90b51171d388583b23d0a
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-04-09 14:10:19 +0530

    Update Union Of Two Sorted Arrays.java

--- Sorting/Union Of Two Sorted Arrays.java
+++ Sorting/Union Of Two Sorted Arrays.java
@@ -14,3 +14,23 @@ while(i<n){
 while(j<m){
   res.add(arr2[j++]);
 }
+
+
+// if the question is of giving elements without repeting then following is the code
+import java.util.*;
+public class Solution {
+    public static List< Integer > sortedArray(int []a, int []b) {
+       List<Integer>res=new ArrayList<>();
+       HashMap<Integer,Integer>ans=new HashMap<>();
+       for(int i=0;i<a.length;i++){
+          ans.put(a[i], ans.getOrDefault(a[i], 0)+1);
+       }
+       for(int j=0;j<b.length;j++){
+          ans.put(b[j], ans.getOrDefault(b[j],0)+1);
+       }
+       for(Map.Entry<Integer,Integer>mapElement:ans.entrySet()){
+           res.add(mapElement.getKey());
+       }
+       return res;
+    }
+}
