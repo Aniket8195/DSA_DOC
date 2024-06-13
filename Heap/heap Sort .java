71e218ba3ce0a89d244c0ff393be3ea9c343016d
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-06-13 19:09:33 +0530

    Create heap Sort .java

---
+++ Heap/heap Sort .java
@@ -0,0 +1,45 @@
+
+class Solution
+{
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)
+    {
+        // Your code here
+        for(int i=(n-2)/2;i>=0;i--){
+            heapify(arr,n,i);
+        }
+    }
+ 
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)
+    {
+        // Your code here
+        int left=2*i+1;
+        int right=2*i+2;
+        int largest=i;
+        if(left<n && arr[left]>arr[largest]){
+            largest=left;
+        }
+        if(right<n && arr[right]>arr[largest]){
+            largest=right;
+        }
+        if(i!=largest){
+            int temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+            heapify(arr,n,largest);
+        }
+    }
+    
+    //Function to sort an array using Heap Sort.
+    public void heapSort(int arr[], int n)
+    {
+        buildHeap(arr,n);
+        for(int i=n-1;i>=0;i--){
+            int temp=arr[0];
+            arr[0]=arr[i];
+            arr[i]=temp;
+            heapify(arr,i,0);
+        }
+    }
+ }
