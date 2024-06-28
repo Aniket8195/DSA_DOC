98c33be5c71c72680b7eb2c6b5b724d297400e96
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-06-28 11:39:35 +0530

    Update 39. Combination Sum.java

--- BackTracking/39. Combination Sum.java
+++ BackTracking/39. Combination Sum.java
@@ -8,18 +8,18 @@ class Solution {
         helper(res, current, target, candidates, 0);
         return res;
     }
-    private void helper(List<List<Integer>> res, List<Integer> current, int target, int[] candidates, int startIndex) {
+    private void helper(List<List<Integer>> res, List<Integer> current, int target, int[] candidates, int i) {
         if (target == 0) {
             res.add(new ArrayList<>(current));
             return;
         }
-        if (target < 0) {
+        if (target < 0 || i>=candidates.length) {
             return;
         }
-        for (int i = startIndex; i < candidates.length; i++) {
-            current.add(candidates[i]);
-            helper(res, current, target - candidates[i], candidates, i);
-            current.remove(current.size() - 1);
-        }
+        current.add(candidates[i]);
+         helper(res, current, target - candidates[i], candidates, i);
+         current.remove(current.size() - 1);
+         helper(res, current, target, candidates, i+1);
+       
     }
 }
