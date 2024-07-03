a092f93dd100d5bbb9756849a08cfb1c0ba99c8a
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-03 18:51:27 +0530

    Create 1614. Maximum Nesting Depth of the Parentheses.java

---
+++ String/1614. Maximum Nesting Depth of the Parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count=0;
+        int res=0;
+        for(int i=0;i<s.length();i++){
+           if(s.charAt(i)=='('){
+             count++;
+           }
+           if(s.charAt(i)==')'){
+            count--;
+           }
+           res=Math.max(count,res);
+        }
+        return res;
+    }
+}
