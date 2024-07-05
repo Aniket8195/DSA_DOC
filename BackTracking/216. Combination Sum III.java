53490e0029480d2428b79be46c58d6b97fabb83f
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-05 19:33:06 +0530

    Update 216. Combination Sum III.java

--- BackTracking/216. Combination Sum III.java
+++ BackTracking/216. Combination Sum III.java
@@ -1,3 +1,30 @@
+#approach 1
+    class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>>res=new ArrayList<>();
+        List<Integer>cur=new ArrayList<>();
+            solve(res,cur,k,n,1,0);
+            return res;
+    }
+    private void solve(List<List<Integer>>res,List<Integer>cur,int k,int n,int i,int sum){
+       if(cur.size()==k){
+           if(sum==n){
+              res.add(new ArrayList<>(cur));
+           }
+           return;
+       }
+       if(i>=10)return;
+       cur.add(i);
+       solve(res,cur,k,n,i+1,sum+i);
+       cur.remove(cur.size()-1);
+       solve(res,cur,k,n,i+1,sum);
+    }
+}
+
+
+
+
+#approch 2
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>>res=new ArrayList<>();
