8d4a84ccae4340b0cdaabf2f5c69766dce5879ab
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-18 15:45:01 +0530

    Create 930. Binary Subarrays With Sum.java

---
+++ Sliding Window/930. Binary Subarrays With Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    int check(int[] nums,int goal){
+        int n=nums.length;
+        if(goal<0)
+            return 0;
+        int l=0,r=0,c=0,s=0;
+        while(r<n){
+            s+=nums[r];
+            while(s>goal){
+                s-=nums[l];
+                l++;
+            }
+            c+=r-l+1;
+            r++;
+        }
+        return c;
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return check(nums,goal)-check(nums,goal-1);  
+    }
+}
