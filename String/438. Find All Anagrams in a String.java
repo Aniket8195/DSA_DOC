21d09c892ecc8533267925a83648f63db0bb6b0d
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-20 08:04:56 +0530

    Create 438. Find All Anagrams in a String.java

---
+++ String/438. Find All Anagrams in a String.java
@@ -0,0 +1,43 @@
+class Solution {
+    public static List<Integer> findAnagrams(String s, String p) {
+        // base case
+        if (s.length() < p.length()) {
+            return List.of();
+        }
+
+        int pLen = p.length();
+        final List<Integer> ans = new ArrayList<>();
+        int[] arr = new int[26];
+        int[] arr2 = new int[26];
+
+        for (int m = 0; m < pLen; m++) {
+            arr2[p.charAt(m) - 'a']++;
+        }
+
+        // verify and store first pLen characters
+        for (int i = 0; i < pLen ; i++) {
+            // arr will have the ASCII values  till pLen.
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        getUpdatedAns(ans, arr, arr2, p, 0);
+
+        for (int j = 0; j < s.length() - pLen; j++) {
+            // remove first character from the array.
+            arr[s.charAt(j) - 'a']--;
+            arr[s.charAt(j + pLen) - 'a']++;
+
+            getUpdatedAns(ans, arr, arr2,  p, j+1);
+        }
+
+        return ans;
+
+    }
+
+    private static void getUpdatedAns(final List<Integer> ans, final int[] arr, final int[] arr2, final String  p, int i) {
+        if (Arrays.equals(arr, arr2)) {
+            ans.add(i);
+        }
+    }
+
+}
