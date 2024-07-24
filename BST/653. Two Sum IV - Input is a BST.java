7a612b206124fbcf13ee831e8c9d75513a0a10be
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-24 13:57:47 +0530

    Create 653. Two Sum IV - Input is a BST.java

---
+++ BST/653. Two Sum IV - Input is a BST.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> result = new ArrayList<>();
+        inorderTraversal(root, result);
+
+        int left = 0, right = result.size() - 1;
+        while (left < right) {
+            int sum = result.get(left) + result.get(right);
+            if (sum == k) {
+                return true;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return false;
+    }
+
+    private void inorderTraversal(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            inorderTraversal(root.left, result);
+            result.add(root.val);
+            inorderTraversal(root.right, result);
+        }
+    }
+}
