2a322d2c9b5c2e5a0c1f420240a958bdf3a0bc10
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-25 00:15:16 +0530

    Create 130. Surrounded Regions.java

---
+++ Graphs/130. Surrounded Regions.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n=board.length;
+        int m=board[0].length;
+        boolean vis[][]=new boolean[n][m];
+        int check[][]=new int[n][m];
+        int i,j;
+        for( i=0;i<n;i++){
+         dfs(check,board,n,m,i,0,vis);
+        }
+        for( j=0;j<m;j++){
+          dfs(check,board,n,m,0,j,vis);
+        }
+        for( i=0;i<n;i++){
+         dfs(check,board,n,m,i,m-1,vis);
+        }
+        for( j=0;j<m;j++){
+          dfs(check,board,n,m,n-1,j,vis);
+        }
+         for(i= 0;i<n;i++)
+        {
+            for(j =0;j<m;j++)
+            {
+                if(board[i][j] == 'O' && check[i][j] == 0 )
+                board[i][j] = 'X';
+            }
+        }
+    }
+    private void dfs(int check[][],char [][]b,int n,int m,int i,int j , boolean visited[][]){
+          if(i<0 || j<0 || i>=n || j>=m)
+        return;
+        if(b[i][j] == 'X' || visited[i][j])
+        return;
+ 
+        check[i][j] = 1;
+        visited[i][j] = true;
+        dfs(check,b,n,m,i+1,j,visited);
+        dfs(check,b,n,m,i-1,j,visited);
+        dfs(check,b,n,m,i,j+1,visited);
+        dfs(check,b,n,m,i,j-1,visited);
+    }
+}
