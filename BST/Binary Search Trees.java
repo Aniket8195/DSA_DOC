9914377d000760b76d4c20780e478a1486be37b5
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-24 09:55:19 +0530

    Create Binary Search Trees.java

---
+++ BST/Binary Search Trees.java
@@ -0,0 +1,15 @@
+class Solution {
+    static boolean isBSTTraversal(int A[]) {
+        // code here
+          int n=A.length; boolean flag = false;
+        for(int i=0;i<n-1;i++){
+            if(A[i]<A[i+1])
+                flag = true;
+            else{
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
