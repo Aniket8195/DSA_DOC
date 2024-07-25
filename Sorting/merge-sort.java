7aa58702eed41d7a5be9fdab75ae1e48838cbf82
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-07-25 14:35:47 +0530

    Update merge-sort.java

--- Sorting/merge-sort.java
+++ Sorting/merge-sort.java
@@ -46,3 +46,54 @@ public class Solution {
         }
     }
 }
+
+
+
+
+// second approach without using extra space
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        return nums;
+    }
+    private void mergeSort(int nums[],int low,int high){
+       if(low<high){
+         int mid=low+(high-low)/2;
+         mergeSort(nums,low,mid);
+         mergeSort(nums,mid+1,high);
+         merge(nums,low,mid,high);
+       }
+    }
+    private void merge(int nums[],int low,int mid,int high){
+        int n1=mid-low+1;
+        int n2=high-mid;
+        int left[]=new int[n1];
+        int right[]=new int[n2];
+        for(int i=0;i<n1;i++){
+         left[i]=nums[low+i];
+        }
+        for(int i=0;i<n2;i++){
+        right[i]=nums[mid+1+i];
+        }
+        int i=0;
+        int j=0;
+        int k=low;
+        while(i<n1&& j<n2){
+              if(left[i]<right[j]){
+                nums[k++]=left[i++];
+              }else{
+                nums[k++]=right[j++];
+              }
+        }
+        while(i<n1){
+          
+                nums[k++]=left[i++];
+              
+        }
+        while(j<n2){
+             nums[k++]=right[j++];
+        }
+
+    }
+}
