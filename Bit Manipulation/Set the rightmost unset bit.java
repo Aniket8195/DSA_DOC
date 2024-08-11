8fd73f230d6edc6701f5b88dd67462547f6fdb8f
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-08-11 16:26:06 +0530

    Update Set the rightmost unset bit.java

--- Bit Manipulation/Set the rightmost unset bit.java
+++ Bit Manipulation/Set the rightmost unset bit.java
@@ -1,6 +1,6 @@
 class Solution {
     static int setBit(int n) {
         // code here
-         return n | (n + 1);
+         return n | (n + 1); // can do as find the position of set bit and then take (n|i<<j) where j is the position of set bit from right (striver solution)
     }
 }
