9d42ee292e64663a62f8890d0a73251cc2d3b914
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2024-08-13 13:27:21 +0530

    Create Find Kth Rotation.java

---
+++ BS/Find Kth Rotation.java
@@ -0,0 +1,34 @@
+class Solution {
+    int findKRotation(int arr[], int n) {
+        // code here
+          
+        int low=0;
+        int high=n-1;
+        int minIdx=0;
+        while(low<=high)
+        {
+            if(arr[low]<=arr[high])
+            {
+                if(arr[minIdx]>arr[low])
+                     minIdx=low;
+                break;
+            }
+            
+            int mid=(low+high)/2;
+            if(arr[low]<=arr[mid])
+            {
+                if(arr[minIdx]>arr[low])
+                     minIdx=low;
+                low=mid+1;
+            }
+            else
+            {
+                if(arr[minIdx]>arr[mid])
+                     minIdx=mid;
+               high=mid-1;
+            }
+        }
+        
+        return minIdx;
+    }
+}
