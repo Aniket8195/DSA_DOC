6d4f27d8e401570abd27a236542f58058e38c6a7
Aniket Patil <114986841+Aniket8195@users.noreply.github.com>
2024-08-18 23:47:49 +0530

    Create Postfix to Prefix Conversion.java

---
+++ Prefix, Infix, PostFix Conversion Problems/Postfix to Prefix Conversion.java
@@ -0,0 +1,16 @@
+class Solution {
+    static String postToPre(String post_exp) {
+        // code here
+           Stack <String> s= new Stack<>();
+        for(int i=0;i<post_exp.length();i++){
+            char c= post_exp.charAt(i);
+            if(Character.isLetter(c))s.push(String.valueOf(c));
+            else{
+                String v=s.pop();
+                s.push(c+s.pop()+v);
+                
+            }
+        }
+        return s.pop();
+    }
+}
