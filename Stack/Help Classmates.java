5ffe3f0938b7accdc3f1a0619b86551c35c5e614
Aniket Patil <114986841+Aniket8195@users.noreply.github.com>
2024-09-11 09:20:40 +0530

    Create Help Classmates.java

---
+++ Stack/Help Classmates.java
@@ -0,0 +1,18 @@
+class Solution {
+	public static int[] help_classmate(int arr[], int n) 
+	{ 
+	    // Your code goes here
+	    Stack<Integer>st=new Stack<>();
+	    int ans[]=new int[arr.length];
+	    Arrays.fill(ans,-1);
+	    for(int i=0;i<arr.length;i++){
+	        while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+	            int x=st.peek();
+	            st.pop();
+	            ans[x]=arr[i];
+	        }
+	        st.push(i);
+	    }
+	    return ans;
+	} 
+}
