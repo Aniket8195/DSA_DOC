dfbc220e02239435fc8b12dcfc6a37ab6c110b3a
Aniket Patil <114986841+Aniket8195@users.noreply.github.com>
2024-09-23 14:48:16 +0530

    Update 176. Second Highest Salary.sql

--- SQL/176. Second Highest Salary.sql
+++ SQL/176. Second Highest Salary.sql
@@ -1,4 +1,13 @@
+select
+(select Salary 
+from Employee 
+group by salary 
+order by salary desc
+limit 1 offset 1) 
+as SecondHighestSalary;
 
+
+# 2nd solution
 select
 (select distinct Salary 
 from Employee order by salary desc 
